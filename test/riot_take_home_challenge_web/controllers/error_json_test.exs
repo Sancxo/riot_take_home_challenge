565c4ef76a8dd2abf16495f26311338bc60bc20b
Simon Tirant <simon.tirant@gmail.com>
2026-01-05 13:41:23 +0100

    chore: mix format

--- test/riot_take_home_challenge_web/controllers/error_json_test.exs
+++ test/riot_take_home_challenge_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule RiotTakeHomeChallengeWeb.ErrorJSONTest do
   use RiotTakeHomeChallengeWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert RiotTakeHomeChallengeWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert RiotTakeHomeChallengeWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
